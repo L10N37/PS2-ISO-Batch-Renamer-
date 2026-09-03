f592ec08ada597a91164ceea1a36c9ca02433cc2
VajskiDs <87033268+L10N37@users.noreply.github.com>
2026-09-03 17:03:35 +1000

    Test audited database uniqueness and titles

--- tests/core_tests.cpp
+++ tests/core_tests.cpp
@@ -14,6 +14,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <utility>
 #include <vector>
 
 namespace fs = std::filesystem;
@@ -81,6 +82,68 @@ void databaseTests() {
 
     ps2br::GameDatabase built_in;
     expect(built_in.loadBuiltIn(error), "built-in database should load: " + error);
+    expect(built_in.recordCount() == 13740, "built-in database should contain the audited record count");
+    expect(built_in.uniqueIdCount() == 13740, "every built-in database ID should be unique");
+    expect(built_in.duplicateIdCount() == 0, "built-in database should contain no duplicate IDs");
+    constexpr std::array<std::pair<std::string_view, std::string_view>, 52> audited_titles{{
+        {"PCPX_966.49", "Gran Turismo 4 - First Preview"},
+        {"SCAJ_200.12", "Venus & Braves"},
+        {"SCKA_200.58", "Action Romance Bumpy Trot"},
+        {"SCKA_200.62", "Saru! Get You! 3"},
+        {"SLAJ_250.23", "Shin Sangoku Musou 3 - Moushouden"},
+        {"SLES_503.82", "Silent Hill 2"},
+        {"SLES_512.27", "Lara Croft Tomb Raider - The Angel of Darkness"},
+        {"SLES_512.52", "The Lord of the Rings - The Two Towers"},
+        {"SLES_512.58", "007 - Nightfire [Platinum]"},
+        {"SLES_518.97", "The Simpsons - Hit & Run"},
+        {"SLES_520.05", "007 - Everything or Nothing"},
+        {"SLES_525.88", "Mercenaries - Playground of Destruction"},
+        {"SLES_525.90", "Mercenaries - Playground of Destruction"},
+        {"SLES_528.01", "The Lord of the Rings - The Third Age"},
+        {"SLES_529.74", "GoldenEye - Rogue Agent"},
+        {"SLES_533.42", "Cricket 2005"},
+        {"SLES_533.57", "21 Card Games"},
+        {"SLES_533.66", "Killer7"},
+        {"SLES_540.16", "AND 1 Streetball"},
+        {"SLES_541.47", "Ice Age 2 - The Meltdown"},
+        {"SLES_542.09", "The Sopranos - Road to Respect"},
+        {"SLES_543.47", "The Sims 2 - Pets"},
+        {"SLES_548.15", "The Legend of Spyro - The Eternal Night"},
+        {"SLES_549.98", "Mercenaries 2 - L'enfer des Favelas"},
+        {"SLES_820.18", "Cy Girls (Ice Disc)"},
+        {"SLES_820.20", "Cy Girls (Ice Disc)"},
+        {"SLES_820.21", "Cy Girls (Aska Disc)"},
+        {"SLKA_251.03", "Shinseiki Evangelion 2 - Evangelions"},
+        {"SLKA_251.31", "Jyuouki - Project Altered Beast"},
+        {"SLKA_253.17", "Jin Samguk Mussang 3"},
+        {"SLPM_650.40", "The Fear [Disc 1]"},
+        {"SLPS_251.95", "Venus & Braves - Majo to Megami to Horobi no Yogen [Premium Box]"},
+        {"SLPS_251.96", "Venus & Braves - Majo to Megami to Horobi no Yogen"},
+        {"SLPS_254.85", "Kidou Senshi Gundam - Ver. 1.5 [Gundam the Best]"},
+        {"SLPS_732.36", "Venus & Braves - Majo to Megami to Horobi no Yogen [PlayStation 2 The Best]"},
+        {"SLES_546.04", "\xC2\xA1Qu\xC3\xA9 Pasa Neng! El Videojuego"},
+        {"SLPM_550.11", "Poi Hito Natsu no Keiken! [Limited Edition]"},
+        {"SLPM_550.12", "Ppoi! Hito Natsu no Keiken!"},
+        {"SLPM_551.32", "Kira Kira - Rock 'N' Roll Show [First Print Limited Edition]"},
+        {"SLPM_551.33", "Kira Kira - Rock 'n' Roll Show"},
+        {"SLPM_551.49", "Loveroot Zero - Kiss Kiss Labyrinth"},
+        {"SLPM_552.79", "Otometeki Koi Kakumei - Love Revo!! [Best Edition]"},
+        {"SLPM_552.88", "Shin Koihime Musou - Otome Ryouran Sangokushi Engi"},
+        {"SLPM_620.93", "Simple 2000 Series Ultimate Vol. 1 - Love Smash!"},
+        {"SLPS_200.61", "Mamimume Mogacho no Print Hour"},
+        {"SLPS_258.83", "Katekyoo Hitman Reborn! Nerae! Ring x Vongola Trainers"},
+        {"SLPS_258.99", "Katekyoo Hitman Reborn! Let's Ansatsu! Nerawareta 10-daime! [Best Collection]"},
+        {"SLPS_259.09", "Hisshou Pachinko Pachi-Slot Kouryaku Series Vol. 13 - Shin Seiki Evangelion - Yakusoku no Toki"},
+        {"SLPS_259.11", "Hisshou Pachinko Pachi-Slot Kouryaku Series Vol. 11 - Shin Seiki Evangelion - Magokoro o, Kimi ni"},
+        {"SLPS_259.31", "Katekyoo Hitman Reborn! Nerae! Ring x Vongola Trainers [Best Collection]"},
+        {"SLPS_259.42", "Hisshou Pachinko Pachi-Slot Kouryaku Series Vol. 14 - CR Shin Seiki Evangelion - Saigo no Shisha"},
+        {"SLPS_259.43", "Hisshou Pachinko Pachi-Slot Kouryaku Series Vol. 14 - CR Shin Seiki Evangelion - Saigo no Shisha [Limited Special Box]"},
+    }};
+    for (const auto& [game_id, expected_title] : audited_titles) {
+        const auto* audited = built_in.findFirst(game_id);
+        expect(audited != nullptr && audited->title == expected_title,
+               "built-in database should contain the audited title for " + std::string(game_id));
+    }
     constexpr std::array<std::string_view, 36> unofficial_ids{
         "USGR_000.00", "USGR_000.01", "USGR_000.02", "USGR_000.03", "USGR_000.04", "USGR_000.05",
         "USGR_000.06", "USGR_000.07", "USGR_000.08", "USGR_000.09", "USGR_000.10", "USGR_000.11",
